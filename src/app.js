253a09acfdf1da1c0bc608a2f4f5b8c4e074619d
lauretteroy <laurieroy.dev@gmail.com>
2021-04-07 09:42:48 -0600

    git init

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import IndecisionApp from './components/IndecisionApp';
-
 import 'normalize.css';
 import './styles/styles.scss'
-// import validator from 'validator';
-// const template = React.createElement('p', {}, 'test');
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+ReactDOM.render(<p>React16 Boilerplate</p>, document.getElementById('app'));
 
