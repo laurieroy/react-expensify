30d43a4f3a2f87803b12c23deeeda3b1b5f14391
lauretteroy <laurieroy.dev@gmail.com>
2021-04-09 14:24:51 -0600

    Add react-redux, render individual expense

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { addExpense }  from './actions/expenses';
@@ -18,8 +19,18 @@ store.dispatch(setTextFilter('water'));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-// console.log(state);
 
-ReactDOM.render(<AppRouter /> , document.getElementById('app'));
+setTimeout(() => {
+	store.dispatch(setTextFilter('bill'));
+}, 3000)
+
+console.log(visibleExpenses);
+
+const jsx = (
+	<Provider store ={store}>
+		<AppRouter />
+	</Provider>
+);
+
+ReactDOM.render(jsx , document.getElementById('app'));
 
