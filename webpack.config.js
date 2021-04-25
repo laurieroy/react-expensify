fe5cf74b2ea9f8a21ada9891157a92821503d44d
lauretteroy <laurieroy.dev@gmail.com>
2021-04-24 19:03:38 -0600

    Change main/index to true pckg.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const path = require("path");
+const webpack = require("webpack");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-	mode: "development",
-	entry: "./src/index.js",
-	// entry: "./src/playground/hoc.js",
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		filename: "main.bundle.js"
-	},
-	module: {
-		rules: [{
-			loader: "babel-loader",
-			test: /\.js$/,
-			exclude: /node_modules/
-		}, {
-			test: /\.s?css$/,
-			use: [
-				"style-loader",
-				"css-loader",
-				"sass-loader"
-			]
-		}]
-	},
-	devtool: "cheap-module-eval-source-map",
-	devServer: {
-		contentBase:  path.join(__dirname, "public"),
-		historyApiFallback:true
-	}
+module.exports = (env) => {
+  const isProduction = env === "production";
+  return {
+    entry: "./src/app.js",
+    // entry: './src/playground/hoc.js',
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.s?css$/,
+          use: MiniCssExtractPlugin.loader({
+            use: ["css-loader", "sass-loader"],
+          }),
+        },
+      ],
+    },
+    // plugins: [
+    //   new MiniCssExtractPlugin({
+    //     filename: "styles.css",
+    //   }),
+    // ],
+    devtool: isProduction ? "source-map" : "cheap-module-eval-source-map",
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true,
+    },
+  };
 };
